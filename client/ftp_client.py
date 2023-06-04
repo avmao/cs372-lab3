7cb128041f4dcdba9a7ea749a6dfa68213328eb8
avmao <ninetymao@gmail.com>
2023-06-03 19:42:39 -0700

    list and remove

--- client/ftp_client.py
+++ client/ftp_client.py
@@ -8,38 +8,47 @@ import asyncio
 
 IP, DPORT = 'localhost', 8080
 
-async def recv_short_msg(reader: asyncio.StreamReader):
-    full_data = await reader.readline()
+def to_hex(number):
+    assert number <= 0xffffffff, "Number too large"
+    return "{:08x}".format(number)
+
+async def send_msg(writer, msg):
+    writer.write(to_hex(len(msg)).encode())
+    writer.write(msg.encode())
+
+    await writer.drain()
+
+async def recv_msg(reader: asyncio.StreamReader):
+    data_length_hex = await reader.readexactly(8)
+    data_length = int(data_length_hex, 16)
+
+    full_data = await reader.readexactly(data_length)
     return full_data.decode()
-
-async def send_pwd(writer, pwd):
-    writer.write(pwd.encode())
-    await writer.drain()
-
-async def send_func(writer, func):
-    writer.write(func.encode())
-    await writer.drain()
 
 async def connect(i):
     reader, writer = await asyncio.open_connection(IP, DPORT)
 
     for i in range(3):
-        pwd_prompt = await recv_short_msg(reader)       # 1
-        print(pwd_prompt)
+        pwd_prompt = await recv_msg(reader)         # 1
+        print(pwd_prompt.rstrip())
         pwd = input()
-        await send_pwd(writer, pwd+"\n")                     # 2
+        await send_msg(writer, pwd+"\n")            # 2
         
-        confirm = await recv_short_msg(reader)          # 3
+        confirm = await recv_msg(reader)            # 3
         if confirm == "Welcome to the server.\n":
             break 
  
     print(confirm)
-    while confirm != "Close":
-        func_prompt = await recv_short_msg(reader)
-        print (func_prompt)
+
+    func = ""
+
+    while func != "close":
+        func_prompt = await recv_msg(reader)            # 4
+        print (func_prompt.strip() + " " , end="")
         func = input()
-        await send_func(writer, func+"\n")
-        confirm = await recv_short_msg(reader)
+        await send_msg(writer, func)                    # 5
+        contents = await recv_msg(reader)               # 6
+        print(contents)
 
     return 0
 
--- server/ftp_server.py
+++ server/ftp_server.py
@@ -7,25 +7,30 @@ os.chdir('myfiles')
 
 INTERFACE, SPORT = 'localhost', 8080
 CHUNK = 100
-CORRECT_PWD = "lab3cs372\n"
+CORRECT_PWD = "a\n"
 PATH = os.getcwd()
-LIST_FILES = "list\n"
+LIST_FILES = "list"
 PUT_FILE = "put file\n"
 GET_FILE = "get file\n"
-REMOVE_FILE = "remove file\n"
-CLOSE = "close\n"
+REMOVE_FILE = "remove"
+CLOSE = "close"
 
-async def send_short_msg(writer, msg):
+def to_hex(number):
+    assert number <= 0xffffffff, "Number too large"
+    return "{:08x}".format(number)
+
+async def send_msg(writer, msg):
+    writer.write(to_hex(len(msg)).encode())
     writer.write(msg.encode())
+
     await writer.drain()
 
-async def recv_pwd(reader: asyncio.StreamReader):
-    full_data = await reader.readline()
-    return full_data.decode()
+async def recv_msg(reader: asyncio.StreamReader):
+    data_length_hex = await reader.readexactly(8)
+    data_length = int(data_length_hex, 16)
 
-async def recv_func(reader: asyncio.StreamReader):
-    which_function = await reader.readline()
-    return which_function.decode()
+    full_data = await reader.readexactly(data_length)
+    return full_data.decode()
 
 async def put_func(reader: asyncio.StreamReader):
     print("Put")
@@ -36,42 +41,44 @@ async def get_func(reader: asyncio.StreamReader):
 async def remove_func(reader: asyncio.StreamReader):
     print("Remove")
 
+
 async def handle_client(reader, writer):
 
     for i in range(3):
-        await send_short_msg(writer, "Enter password:\n")        # 1
-        pwd = await recv_pwd(reader)                            # 2
+        await send_msg(writer, "Enter password: \n")               # 1
+        pwd = await recv_msg(reader)                            # 2
         print("received " + pwd)
         if pwd == CORRECT_PWD:
-            await send_short_msg(writer, "Welcome to the server.\n")     # 3a
+            await send_msg(writer, "Welcome to the server.\n")     # 3a
             break
         else: 
-            await send_short_msg(writer, "Incorrect password.\n")        # 3b
+            await send_msg(writer, "Incorrect password.\n")        # 3b
 
     if pwd != CORRECT_PWD:
-        await send_short_msg(writer, "NAK Unable to connect to client: too many incorrect passwords.\n") 
+        await send_msg(writer, "NAK Unable to connect to client: too many incorrect passwords.\n") 
         quit()
-
-    await send_short_msg(writer, "What function to perform?\n")
-    fcn = await recv_func(reader)    
-    print("received " + fcn)
-          
-    if fcn == LIST_FILES:
-        #dir_list = os.listdir(PATH)
-        #await send_short_msg(writer, "ACK ")
-        #printlist = print(dir_list)
-        #await send_short_msg(writer, printlist)
-        print("test")
-    elif fcn == PUT_FILE:
-        put_func(reader)
-    elif fcn == GET_FILE:
-        get_func(reader)
-    elif fcn == REMOVE_FILE:
-        remove_func(reader) 
-    elif fcn == CLOSE:
-        writer.close()
-    else:
-         await send_short_msg(writer, "Invalid function.\n")
+        
+    while 1:
+        await send_msg(writer, "ftp> \n")                       # 4
+        input = await recv_msg(reader)                            # 5 
+        fcn = input.split(" ",1)
+        print("received " + str(fcn))
+        if fcn[0] == LIST_FILES:
+            dir_list = os.listdir(PATH)
+            await send_msg(writer, "ACK\n" + str(dir_list))         # 6a
+        elif fcn[0] == PUT_FILE:
+            put_func(reader)                                    # 6b
+        elif fcn[0] == GET_FILE:
+            get_func(reader)                                    # 6c
+        elif fcn[0] == REMOVE_FILE:
+            os.remove(fcn[1])                                     # 6d
+            print("removed " + fcn[1])
+            await send_msg(writer, "ACK\n")
+        elif fcn[0] == CLOSE:
+            asyncio.get_event_loop().stop()                     # 6e
+            break
+        else:
+            await send_msg(writer, "Invalid function.\n")       # 6f
           
     writer.close()
     await writer.wait_closed()
